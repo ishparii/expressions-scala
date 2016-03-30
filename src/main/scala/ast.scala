bcd63172349212805f9bf115f2e60aaf5e800dc4
cpatel10 <cpatel10@luc.edu>
2016-03-30 17:13:50 -0500

    changes in ast

--- src/main/scala/ast.scala
+++ src/main/scala/ast.scala
@@ -9,3 +9,9 @@ case class Minus(left: Expr, right: Expr) extends Expr
 case class Times(left: Expr, right: Expr) extends Expr
 case class Div(left: Expr, right: Expr) extends Expr
 case class Mod(left: Expr, right: Expr) extends Expr
+
+
+case class Block(expressions:Expr*) extends Expr
+case class Conditional(guard:Expr, ifBranch:Expr, elseBranch:Expr) extends Expr
+case class Loop(guard:Expr, body:Expr) extends Expr
+case class Assignment(left:String, right:Expr) extends Expr
